5be5034e6427e949cd24015e9c486c68e6461701
andradean <viniciusansilva@outlook.com>
2023-01-24 12:10:15 -0300

    exerci operacoes

---
+++ ex004.py
@@ -0,0 +1,8 @@
+n1 = int(input('insira um numero inteiro: '))
+na = n1 - 1
+ns = n1 + 1
+
+print(f'o antecessor do seu numero é: {na}\no sucessor do seu numero é: {ns}')
+
+
+
---
+++ ex005.py
@@ -0,0 +1,5 @@
+n1 = float(input('digite um numero: '))
+nd = n1 * 2
+nt = n1 * 3
+nr = n1 ** (1/2)
+print(f'o numero é: {n1}, o dobro dele é igual a {nd}, o triplo é igual a {nt}, e sua raiz é {nr}')
